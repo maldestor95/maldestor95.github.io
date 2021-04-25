e4ac3379ea64664d576fb43a1277209a7c6d749a
Ludovic Deparis <maldestor95@gmail.com>
2021-04-25 08:22:37 +0200

    init document

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,13 @@
     <v-app-bar app color="primary" dark>
         <div class="d-flex align-center">
             <v-img alt="Vuetify Logo" class="shrink mr-2" contain src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png" transition="scale-transition" width="40" />
-
+            LISY
             <v-img alt="Vuetify Name" class="shrink mt-1 hidden-sm-and-down" contain min-width="100" src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png" width="100" />
         </div>
 
         <v-spacer></v-spacer>
 
-        <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
+        <v-btn href="https://github.com/maldestor95/lisy" target="_blank" text>
             <span class="mr-2">Latest Release</span>
             <v-icon>mdi-open-in-new</v-icon>
         </v-btn>
@@ -22,7 +22,7 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,61 +12,14 @@
 
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
+          Welcome to LISY
         </h1>
 
         <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
+          This is a template vuejs site for maldestor95 github pages.
+          Anyway, feel free to fork it.
         </p>
       </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          What's next?
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Important Links
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
       <v-col
         class="mb-5"
         cols="12"
@@ -109,42 +62,9 @@ export default {
         text: 'awesome-vuetify',
         href: 'https://github.com/vuetifyjs/awesome-vuetify',
       },
-    ],
-    importantLinks: [
-      {
-        text: 'Documentation',
-        href: 'https://vuetifyjs.com',
-      },
-      {
-        text: 'Chat',
-        href: 'https://community.vuetifyjs.com',
-      },
-      {
-        text: 'Made with Vuetify',
-        href: 'https://madewithvuejs.com/vuetify',
-      },
-      {
-        text: 'Twitter',
-        href: 'https://twitter.com/vuetifyjs',
-      },
-      {
-        text: 'Articles',
-        href: 'https://medium.com/vuetify',
-      },
-    ],
-    whatsNext: [
-      {
-        text: 'Explore components',
-        href: 'https://vuetifyjs.com/components/api-explorer',
-      },
-      {
-        text: 'Select a layout',
-        href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
-      },
-      {
-        text: 'Frequently Asked Questions',
-        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-      },
+      { text: 'vuejs',
+        href: ''
+      }
     ],
   }),
 };

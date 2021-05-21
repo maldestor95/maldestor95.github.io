b4f5e4257c02067eb44fbda0ef0fe66fd1b25b97
maldestor95 <maldestor95@gmail.com>
2021-05-21 06:54:11 +0200

    pointscounter - test user

--- src/features/pointscounter/scorelib.js
+++ src/features/pointscounter/scorelib.js
@@ -32,20 +32,21 @@ class User {
     return this.score.reduce((a, x, i) => [...a, x + (a[i - 1] || 0)], []);
   }
 }
+module.exports = User;
 
-const testU = new User('Ludo');
-testU.addRound(5);
-testU.addRound(10);
-testU.addRound(15);
-console.log(testU); // User { name: 'Ludo', score: [ 5, 10, 15 ] }
-console.log(testU.round(2)); // 15
-console.log(testU.round(10)); // null
-testU.editRound(1, 0);
-console.log(testU); // User { name: 'Ludo', score: [ 5, 0, 15 ] }
-console.log(testU.cumul()); // User { name: 'Ludo', score: [ 5, 5, 20 ] }
-testU.addRound(10);
-testU.addRound(11);
-testU.addRound(12);// User { name: 'Ludo', score: [ 5, 0, 15, 10, 11, 12 ] }
-testU.deleteRound(2);
-console.log(testU); // User { name: 'Ludo', score: [ 5, 0, 10, 11, 12 ] }
-console.log(testU.deleteRound(5));
+// const testU = new User('Ludo');
+// testU.addRound(5);
+// testU.addRound(10);
+// testU.addRound(15);
+// console.log(testU); // User { name: 'Ludo', score: [ 5, 10, 15 ] }
+// console.log(testU.round(2)); // 15
+// console.log(testU.round(10)); // null
+// testU.editRound(1, 0);
+// console.log(testU); // User { name: 'Ludo', score: [ 5, 0, 15 ] }
+// console.log(testU.cumul()); // User { name: 'Ludo', score: [ 5, 5, 20 ] }
+// testU.addRound(10);
+// testU.addRound(11);
+// testU.addRound(12);// User { name: 'Ludo', score: [ 5, 0, 15, 10, 11, 12 ] }
+// testU.deleteRound(2);
+// console.log(testU); // User { name: 'Ludo', score: [ 5, 0, 10, 11, 12 ] }
+// console.log(testU.deleteRound(5));

4767e0dbc0af707f7c26f69d94fc57f1c96b547c
maldestor95 <maldestor95@gmail.com>
2021-05-29 11:32:20 +0200

    scoreboard - setup

--- src/features/pointscounter/scoreBoard.vue
+++ src/features/pointscounter/scoreBoard.vue
@@ -1,45 +1,56 @@
 <template>
   <div>
-    <section>
-      <v-card>
+      <v-icon @click="setup = !setup">mdi-cogs</v-icon>
+      <v-card v-if="setup">
         <h1>Setup</h1>
         <v-container>
           <v-btn color="success" @click="addplayer()">
             <v-icon>mdi-plus-circle</v-icon>
             Add player
           </v-btn>
-          <v-btn>
+          <v-btn color="success" @click="newGame()" v-if="playerList.length>0">
             <v-icon>mdi-restart-off</v-icon>
             new game
           </v-btn>
-          <v-row v-for="player in playerList" :key="player.id">
+          <v-row v-for="player in playerList" :key="player.uuid">
             <v-col>
               <v-text-field
                 label="nom"
-                v-model="player.nom"
+                v-model="player.name"
               ></v-text-field>
             </v-col>
             <v-col>
-              <v-btn color="success" @click="removePlayer(player.id)">remove player</v-btn>
+              <v-btn color="success" @click="removePlayer(player.uuid)">remove player</v-btn>
             </v-col>
             <v-col>
-
+              {{ player.total}}
             </v-col>
           </v-row>
         </v-container>
       </v-card>
-    </section>
-    <section>
+    <v-card v-if=" !setup ">
       <h1>Score</h1>
+      <v-row v-for="player in playerList" :key="player.uuid">
+        <v-col>{{player.name}}</v-col>
+        <v-col>{{cRound(player.uuid)}}</v-col>
+        <v-col>
+            <v-btn color="success" @click="updateRound(player.uuid, 10)">+10</v-btn>
+            <v-btn color="success" @click="updateRound(player.uuid, 5)">+5</v-btn>
+            <v-btn color="success" @click="updateRound(player.uuid, 1)">+1</v-btn>
+            <v-btn color="success" @click="updateRound(player.uuid, -1)">-1</v-btn>
+            <v-btn color="success" @click="updateRound(player.uuid, -5)">-5</v-btn>
+            <v-btn color="success" @click="updateRound(player.uuid, -10)">-10</v-btn>
+        </v-col>
+      </v-row>
       <div>view current score</div>
       <div>add round</div>
-    </section>
     <section>
       <h1>Graph</h1>
       <div>roundTable</div>
       <div>editround</div>
       <div>deleteround</div>
     </section>
+    </v-card>
   </div>
 </template>
 
@@ -51,36 +62,42 @@ export default {
   },
   data() {
     return {
+      setup: true,
       nbplayer: 3,
       scores: [],
+      roundId: 0,
+      currentRound: [],
     };
   },
   created() {
-    this.scores = new ScoreStore(['joueur 1', 'joueur 2']);
+    this.scores = new ScoreStore([]);
   },
   mounted() {
   },
   methods: {
-    addplayer() {
-      this.scores.addPlayer('nouveau joueur');
+    addplayer(name) {
+      this.scores.addPlayer(name);
     },
-    removePlayer(id) {
-      const { name } = this.playerList.filter((p) => (p.id === id))[0];
+    removePlayer(uuid) {
+      const { name } = this.playerList.filter((p) => (p.uuid === uuid))[0];
       this.scores.deletePlayer(name);
+    },
+    newGame() {
+      const round = this.scores.playerList.map((player) => ({ ...player, round: 0 }));
+      this.currentRound = [...round];
+      this.setup = false;
+    },
+    updateRound(uuid, value) {
+      const uuidRound = this.currentRound.filter((round) => (round.id === uuid))[0];
+      uuidRound.round += value;
+    },
+    cRound(uuid) {
+      return this.currentRound.filter((round) => (round.id === uuid))[0].round;
     },
   },
   computed: {
     playerList() {
-      const pList = [];
-      for (let index = 0; index < this.scores.playerList.length; index += 1) {
-        pList.push(
-          {
-            nom: this.scores.playerList[index],
-            id: index,
-          },
-        );
-      }
-      return pList;
+      return this.scores.store;
     },
   },
 };

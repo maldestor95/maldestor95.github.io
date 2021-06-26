7ff143d139268ac13bc9f998ab9a0440ed280071
Ludovic Deparis <maldestor95@gmail.com>
2021-06-26 09:12:14 +0200

    reorganise scoreboard layout

--- src/features/pointscounter/scoreBoard.vue
+++ src/features/pointscounter/scoreBoard.vue
@@ -1,125 +1,175 @@
 <template>
-  <div>
-    <v-card v-if="showSetup"
-      class="d-flex flex-column">
-      <v-card-title color="primary">
-        <v-icon @click="toggleSetup()">mdi-cogs</v-icon>
-        <h1>Setup</h1>
-      </v-card-title>
-      <v-card-actions>
-        <v-container>
-          <v-row>
-            <v-btn color="success" @click="addplayer()">
-              <v-icon>mdi-plus-circle</v-icon>
-              Add player
-            </v-btn>
-            <v-spacer></v-spacer>
-            <v-btn color="success" @click="newGame()" :disabled="playerList.length===0">
-              <v-icon>mdi-restart-off</v-icon>
-              new game
-            </v-btn>
-            <v-spacer></v-spacer>
-          </v-row>
-        </v-container>
-      </v-card-actions>
-      <v-card-text fluid>
-            <v-container fluid>
-              <v-row class="d-none d-sm-flex">
-                <v-col  cols="1" sm="2"
-                class="ml-n6 ml-sm-0 mr-8 mr-sm-n16"
-                >
-                  action
-                </v-col>
-                <v-col sm="9">
-                  Name
-                </v-col>
-                <v-col cols="1" >
-                  Score
-                </v-col>
-              </v-row>
-              <v-row v-for="player in playerList" :key="player.uuid"
-              dense
-              >
-                <v-col cols="1" sm="2" class="mt-5 ml-n6 ml-sm-0 mr-11 mr-sm-n16">
-                  <v-btn color="success" small >
-                    <v-icon @click="removePlayer(player.uuid)">mdi-delete-circle</v-icon>
-                  </v-btn>
-                </v-col>
-                <v-col cols="10" sm="9" >
-                  <v-text-field
-                    :label= "'Joueur ' + playerNumber(player.uuid)"
-                    v-model="player.name"
-                  ></v-text-field>
-                </v-col>
-                <v-col cols="1" sm="1" class="mt-5 d-none d-sm-inline text-center">
-                  {{ player.total}}
-                </v-col>
+  <v-container >
+    <v-icon @click="toggleSetup()">mdi-cogs</v-icon>
+    <v-row fluid>
+      <!-- Setup -->
+      <v-col v-if="showSetup" >
+        <v-card>
+          <v-card-title color="primary">
+            <h1>Setup</h1>
+          </v-card-title>
+          <v-card-actions>
+            <v-container>
+              <v-row>
+                <v-spacer></v-spacer>
+                <v-btn color="success" @click="addplayer()">
+                  <v-icon>mdi-plus-circle</v-icon>
+                  Add player
+                </v-btn>
+                <v-spacer></v-spacer>
+                <v-btn color="success" @click="newGame()" :disabled="playerList.length===0">
+                  <v-icon>mdi-restart-off</v-icon>
+                  new
+                </v-btn>
+                <v-spacer></v-spacer>
               </v-row>
             </v-container>
-      </v-card-text>
-    </v-card>
-    <v-card v-if=" !showSetup ">
-      <h1>Score</h1>
-      <p>Round {{roundId}}</p>
-      <v-row v-for="player in playerList" :key="player.uuid">
-        <v-col>{{player.name}}</v-col>
-        <v-col>{{cRound(player.uuid)}}</v-col>
-        <v-col>
-            <v-btn color="success" @click="updateRound(player.uuid, 10)">+10</v-btn>
-            <v-btn color="success" @click="updateRound(player.uuid, 5)">+5</v-btn>
-            <v-btn color="success" @click="updateRound(player.uuid, 1)">+1</v-btn>
-            <v-btn color="success" @click="updateRound(player.uuid, -1)">-1</v-btn>
-            <v-btn color="success" @click="updateRound(player.uuid, -5)">-5</v-btn>
-            <v-btn color="success" @click="updateRound(player.uuid, -10)">-10</v-btn>
-        </v-col>
-        <v-col>
-          Provisoire {{getScores(player.uuid) + cRound(player.uuid)}}
-        </v-col>
-      </v-row>
-      <v-btn @click="newRound">new round</v-btn>
-    </v-card>
+          </v-card-actions>
+          <v-card-text fluid>
+                <v-container fluid>
+                  <v-row class="d-none d-sm-flex">
+                    <v-col  cols="1" sm="2"
+                    class="ml-n6 ml-sm-0 mr-8 mr-sm-n16"
+                    >
+                      action
+                    </v-col>
+                    <v-col sm="9">
+                      Name
+                    </v-col>
+                    <v-col cols="1" >
+                      Score
+                    </v-col>
+                  </v-row>
+                  <v-row v-for="player in playerList" :key="player.uuid"
+                  dense
+                  >
+                    <v-col cols="1" sm="2" class="mt-5 ml-n6 ml-sm-0 mr-11 mr-sm-n16">
+                      <v-btn color="success" small >
+                        <v-icon @click="removePlayer(player.uuid)">mdi-delete-circle</v-icon>
+                      </v-btn>
+                    </v-col>
+                    <v-col cols="10" sm="9" >
+                      <v-text-field
+                        :label= "'Joueur ' + playerNumber(player.uuid)"
+                        v-model="player.name"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="1" sm="1" class="mt-5 d-none d-sm-inline text-center">
+                      {{ player.total}}
+                    </v-col>
+                  </v-row>
+                </v-container>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <!-- Round -->
+      <v-col v-if=" !showSetup " class="px-0 px-sm-4" cols="12" sm="4">
+        <v-card  max-width="600px" class="px-2" >
+          <h1>Score</h1>
+          <h2>Round {{roundId}}</h2>
+          <v-row >
+              <v-col>name</v-col>
+              <v-col class=" editRoundScore">Round </v-col>
+              <v-col class=" editRoundScore">Score</v-col>
+            </v-row>
+          <div v-for="player in playerList" :key="player.uuid">
+            <v-row >
+              <v-col>{{player.name}}</v-col>
+              <v-col @click="currentPlayer=player.uuid"
+              class=" editRoundScore"
+              >
+              {{cRound(player.uuid)}}
+              </v-col>
+              <v-col class=" editRoundScore">
+                {{getScores(player.uuid) + cRound(player.uuid)}}
+              </v-col>
+            </v-row>
+            <v-row v-show="playerRoundEditable(player.uuid)"
+            class="pb-4">
+              <v-spacer></v-spacer>
+              <v-btn :small="displaySmallDevice" color="green lighten-4"
+                @click="updateRound(player.uuid, 10)">
+                +10
+              </v-btn>
+              <v-spacer></v-spacer>
+              <v-btn :small="displaySmallDevice" color="green lighten-4"
+              @click="updateRound(player.uuid, 5)">
+              +5</v-btn>
+              <v-spacer></v-spacer>
+              <v-btn :small="displaySmallDevice" color="green lighten-4"
+              @click="updateRound(player.uuid, 1)">
+              +1</v-btn>
+              <v-spacer></v-spacer>
 
-    <!-- Table with round score -->
-    <v-card v-if=" !showSetup ">
-      <div>Score history</div>
-      <v-switch label="Cumul" v-model="cumulativeDisplay"></v-switch>
-        <v-simple-table class='roundTable' height='200px'>
-          <template v-slot:default>
-          <thead>
-            <tr>
-              <th class="text-left">
-                Round
-              </th>
-              <th v-for="pl in playerList" :key="pl.name"
-              class="text-left">
-                {{pl.name}}
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr
-              v-for="rId in roundId"
-              :key="rId"
-            >
-              <td> {{ roundId-rId }}</td>
-              <td v-for="pl in playerList" :key="pl.name"
+            </v-row>
+            <v-row v-show="playerRoundEditable(player.uuid)"
+            class="pb-4">
+                <v-spacer></v-spacer>
+                <v-btn :small="displaySmallDevice" color="red lighten-4"
+                @click="updateRound(player.uuid, -1)">
+                -1</v-btn>
+                <v-spacer></v-spacer>
+                <v-btn :small="displaySmallDevice" color="red lighten-4"
+                @click="updateRound(player.uuid, -5)">
+                -5</v-btn>
+                <v-spacer></v-spacer>
+                <v-btn :small="displaySmallDevice" color="red lighten-4"
+                @click="updateRound(player.uuid, -10)">
+                -10</v-btn>
+                <v-spacer></v-spacer>
+            </v-row>
+          </div>
+          <v-row class="mt-5 pb-4 justify-center">
+            <v-btn @click="newRound">new round</v-btn>
+          </v-row>
+        </v-card>
+      </v-col >
+      <!-- Table with round score -->
+      <v-col v-if=" !showSetup " class="px-0 px-sm-4" cols="12" sm="4">
+      <v-card >
+        <h1>Score history</h1>
+        <v-switch label="Cumul" v-model="cumulativeDisplay"></v-switch>
+          <v-simple-table class='roundTable' height='200px'>
+            <template v-slot:default>
+            <thead>
+              <tr>
+                <th class="text-left">
+                  Round
+                </th>
+                <th v-for="pl in playerList" :key="pl.name"
                 class="text-left">
-                {{playerRoundScore(pl.uuid, roundId-rId+1, cumulativeDisplay)}}
-              </td>
-            </tr>
-          </tbody>
-        </template>
-    </v-simple-table>
-      <!-- <div>editround</div>
-      <div>deleteround</div> -->
-    </v-card>
-
-    <v-card v-if=" !showSetup && roundId>0">
-      <linechart chartid= "Line Chart"
-        :data="plotData"
-      ></linechart>
-    </v-card>
-  </div>
+                  {{pl.name}}
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="rId in roundId"
+                :key="rId"
+              >
+                <td> {{ roundId-rId }}</td>
+                <td v-for="pl in playerList" :key="pl.name"
+                  class="text-left">
+                  {{playerRoundScore(pl.uuid, roundId-rId+1, cumulativeDisplay)}}
+                </td>
+              </tr>
+            </tbody>
+          </template>
+      </v-simple-table>
+        <!-- <div>editround</div>
+        <div>deleteround</div> -->
+      </v-card>
+    </v-col>
+    <!-- chart -->
+    <v-col v-if=" !showSetup && roundId>0" class="px-0 px-sm-4" cols="12" sm="4">
+      <v-card >
+        <linechart chartid= "Score Chart"
+          :data="plotData"
+        ></linechart>
+      </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -138,6 +188,7 @@ export default {
       roundId: 0,
       currentRound: [],
       cumulativeDisplay: true,
+      currentPlayer: 0,
     };
   },
   created() {
@@ -210,6 +261,9 @@ export default {
       }
       return playerScores.score[rId];
     },
+    playerRoundEditable(uuid) {
+      return uuid === this.currentPlayer;
+    },
   },
   computed: {
     playerList() {
@@ -230,6 +284,9 @@ export default {
       }
       return result;
     },
+    displaySmallDevice() {
+      return this.$vuetify.breakpoint.name === 'xs';
+    },
   },
 };
 </script>
@@ -240,8 +297,10 @@ export default {
   background-color: red;
   // max-height: 500px;
 }
-.title {
+h1, h2 {
   text-align: center;
 }
-
+.editRoundScore {
+  text-align: center;
+}
 </style>

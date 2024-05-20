0d5831f9b3f6fbf84369ef7263dd62b65718cb59
maldestor95 <maldestor95@gmail.com>
2024-05-20 09:14:21 +0200

    define background and menu

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,34 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import card from "./components/card.vue";
+const links = [
+  {
+    url: "https://maldestor95.github.io/score/",
+    name: "Score Board",
+  },
+  {
+    url: "https://maldestor95.github.io/recipeweb/",
+    name: "Recette",
+  },
+  {
+    url: "https://maldestor95.github.io/dorf/",
+    name: "Dorf Romantik",
+  },
+];
+</script>
 
-<template>HELLO</template>
+<template>
+  <div class="absolute top-0 min-h-screen min-w-full">
+    <div class="relative background mx-auto"></div>
+  </div>
+  <card
+    v-for="item in links"
+    :url="item.url"
+    :target-blank="true"
+    class="relative z-50 w-80"
+  >
+    {{ item.name }}
+  </card>
+</template>
 
 <style scoped>
 .logo {
@@ -14,5 +42,16 @@
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+.background {
+  height: 100vh;
+  @apply bg-red-400 mx-auto;
+  /* @apply md:bg-[length:200px_100px]; */
+  @apply bg-cover;
+  /* background-size: 180%; */
+  background-image: url("../public/backgroundRobot.png");
+  background-repeat: repeat-y;
+  background-position: top;
 }
 </style>
